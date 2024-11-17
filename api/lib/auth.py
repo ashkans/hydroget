f20679a18267606edcb811fc92cff1722e18f39d
Ashkan Shokri <ashkan.shokri@gmail.com>
2024-11-17 14:46:48 +1100

    added error handeling to auth

--- api/lib/auth.py
+++ api/lib/auth.py
@@ -5,5 +5,13 @@ import dotenv
 dotenv.load_dotenv('.env.local')
 
 def user_id_from_token(token: str):
-    res = jwt.decode(token, os.getenv('JWT_PUBLIC_KEY'), algorithms="RS256")
+    public_key = os.getenv('JWT_PUBLIC_KEY')
+    if not public_key:
+        raise Exception('JWT_PUBLIC_KEY is not set')
+    try:
+        res = jwt.decode(token, public_key, algorithms="RS256")
+    except jwt.exceptions.InvalidSignatureError:
+        raise Exception('Invalid token signature')
+    except jwt.exceptions.InvalidTokenError:
+        raise Exception('Invalid token')
     return res['sub']
